b409fd984a045aec2fd97b776e6e18cabc787ccd
Gürkan Arslan <gurkaanima@gmail.com>
2021-04-08 21:51:52 +0300

    Add project files.

---
+++ XamarinSentryTest/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Sentry;
+
+namespace XamarinSentryTest
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            SentryXamarin.Init(options =>
+            {
+                options.Dsn = "https://bb774de7b9b54bd989bdfee00bbe2658@o514648.ingest.sentry.io/5711576";
+            });
+
+
+           //throw null;
+            
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
